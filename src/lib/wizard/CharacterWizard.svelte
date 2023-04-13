43a72f35711c390e807dda5d5a9169945c26a224
Xananth <107520289+Xananth@users.noreply.github.com>
2023-04-13 14:42:28 +1000

    1 file modified

--- src/lib/wizard/CharacterWizard.svelte
+++ src/lib/wizard/CharacterWizard.svelte
@@ -1,5 +1,34 @@
 <script>
 import NCL from "./NCL.svelte"
+
+let character = {
+  name: "",
+  class: "",
+  level: "",
+  guard: "",
+  dive: "",
+  initiative: "",
+  threshold: "",
+  lpace: "",
+  wpace: "",
+  fpace: "",
+  maxHP: "",
+  currentHP: "",
+  maxMana: "",
+  currentMana: "",
+  maxRecovery: "",
+  expendedRecovery: "",
+  manaCeiling: "",
+  demise: "",
+  fatigue: "",
+  power: "",
+  speed: "",
+  forethought: "",
+  instinct: "",
+  panache: "",
+  grace: "",
+  guile: "",
+}
 </script>
 
 <NCL />
